07d9851f856ee86f4cd1c336148716083bba3564
Axel Ef <olek4don@gmail.com>
2022-11-10 23:35:57 +0100

    conditional expressions

--- listcomp.py
+++ listcomp.py
@@ -2,7 +2,9 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
+number = int(input("Please enter a number, and I'll tell you its square: "))
+
 squares = [number ** 2 for number in numbers]
 
-print(squares)
-
+index_pos = numbers.index(number)
+print(squares[index_pos])
--- listfor.py
+++ listfor.py
@@ -2,8 +2,11 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
+number = int(input("Please enter a number, and I'll tell you its square: "))
+
 squares = []
 for number in numbers:
     squares.append(number ** 2)
 
-print(squares)
+index_pos = numbers.index(number)
+print(squares[index_pos])
